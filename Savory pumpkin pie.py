bdb4b937317a8760cb7ebf93d57659bf57517d4f
ElisabettaPigni <elisabetta.pigni@gmail.com>
2024-10-03 13:19:58 +0200

    New new version of the pumpkin pie

--- Savory pumpkin pie.py
+++ Savory pumpkin pie.py
@@ -8,23 +8,23 @@ Created on Thu Oct  3 12:07:42 2024
 INGREDIENTS
 Serves 8
 Prep. time: 25 minutes
-- [] Cooking time: 35 minutes 
-- [] For the crust 
-- [] 300g spelt flour
-- [] 50ml olive oil
-- [] ½ tsp salt
-- [] ice-cold water
+-[] Cooking time: 35 minutes 
+-[] For the crust 
+-[] 300g spelt flour
+-[] 50ml olive oil
+-[] ½ tsp salt
+-[] ice-cold water
 
 FOR THE FILLING
-- [] 180g soft Stracchino cheese
-- [] 150g semi-soft Taleggio cheese
-- [] 400g fresh pumpkin
-- [] 125g champignon mushrooms
-- [] 1 medium onion
-- [] 2 large eggs
-- [] 3 tbsp olive oil
-- [] 40g toasted hazelnuts (can be omitted)
-- [] ground black pepper to taste
-- [] salt to taste
-- [] a cup of water
+-[] 180g soft Stracchino cheese
+-[] 150g semi-soft Taleggio cheese
+-[] 400g fresh pumpkin
+-[] 125g champignon mushrooms
+-[] 1 medium onion
+-[] 2 large eggs
+-[] 3 tbsp olive oil
+-[] 40g toasted hazelnuts (can be omitted)
+-[] ground black pepper to taste
+-[] salt to taste
+-[] a cup of water
 
